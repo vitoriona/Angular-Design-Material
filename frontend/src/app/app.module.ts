9a3e3c68f07b32c6f6015d8590f6bac99d2bb965
Vitor Ionã <vitoriona3000@gmail.com>
2020-05-11 22:22:04 -0300

    Update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,7 +27,12 @@ import { HttpClientModule} from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
 
 // Components, Directives e Pipes
 @NgModule({
@@ -41,6 +46,8 @@ import { MatInputModule } from '@angular/material/input'
     RedDirective,
     ForDirective,
     ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component,
   ],
   imports: [
     BrowserModule,
@@ -55,7 +62,10 @@ import { MatInputModule } from '@angular/material/input'
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
