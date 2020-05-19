4701a7f08f30ff089a091db6c589d8b5fffdb8a8
Vitor Ionã <vitoriona3000@gmail.com>
2020-05-18 23:07:17 -0300

    CRU-D

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -32,7 +32,7 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
-  redById(id: string): Observable<Product> {
+  readById(id: string): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
     return this.http.get<Product>(url)
   }
